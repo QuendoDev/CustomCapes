c53e082aa3862675ea4986f5fa18b02e855d9a85
mrkino99 <58349008+mrkino99@users.noreply.github.com>
2019-12-20 13:33:46 +0000

    Update customcapes.java

--- customcapes.java
+++ customcapes.java
@@ -1,4 +1,10 @@
- String username = player.getNameClear();
+public class CapeUtils
+{
+
+public static void downloadCape(final AbstractClientPlayer player)
+    { 
+
+String username = player.getNameClear();
 
         if (username != null && !username.isEmpty())
         {
@@ -48,4 +54,22 @@
               textureManager.loadTexture(rl, textureCape);
           }
         }
+}
+public static BufferedImage parseCape(BufferedImage img)
+    {
+        int imageWidth = 64;
+        int imageHeight = 32;
+        int srcWidth = img.getWidth();
+
+        for (int srcHeight = img.getHeight(); imageWidth < srcWidth || imageHeight < srcHeight; imageHeight *= 2)
+        {
+            imageWidth *= 2;
+        }
+
+        BufferedImage imgNew = new BufferedImage(imageWidth, imageHeight, 2);
+        Graphics g = imgNew.getGraphics();
+        g.drawImage(img, 0, 0, (ImageObserver)null);
+        g.dispose();
+        return imgNew;
+    }
 }
